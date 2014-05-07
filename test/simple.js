78593b7f282d6591605ab6341f248818578a4d8e
John Clark <socrates37@gmail.com>
2014-05-07 16:49:11 -0600

    simple date complete

--- test/simple.js
+++ test/simple.js
@@ -171,6 +171,217 @@ describe('Simple', function(){
       expect(simple.getHours()).to.equal(12);
     });
 
+    it('should error on +2000-01-01T25', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T25');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T12_30', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12_30');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T12:3', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12:3');
+      }).to.throw(Error);
+    });
+
+    it('should parse +2000-01-01T12:30', function(){
+      var simple = new Simple('+2000-01-01T12:30');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(12);
+      expect(simple.getMinutes()).to.equal(30);
+    });
+
+    it('should error on +2000-01-01T12:60', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12:60');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T12:30_15', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12:30_15');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T12:30:1', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12:30:1');
+      }).to.throw(Error);
+    });
+
+    it('should parse +2000-01-01T12:30:15', function(){
+      var simple = new Simple('+2000-01-01T12:30:15');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(12);
+      expect(simple.getMinutes()).to.equal(30);
+      expect(simple.getSeconds()).to.equal(15);
+    });
+
+    it('should error on +2000-01-01T12:30:60', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12:30:60');
+      }).to.throw(Error);
+    });
+
+    it('should parse +2000-01-01T24:00:00', function(){
+      var simple = new Simple('+2000-01-01T24:00:00');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(24);
+      expect(simple.getMinutes()).to.equal(0);
+      expect(simple.getSeconds()).to.equal(0);
+    });
+
+    it('should error on +2000-01-01T24:10:00', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:10:00');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T24:00:10', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:10');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T12:30:15B', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T12:30:15B');
+      }).to.throw(Error);
+    });
+
+    it('should parse +2000T12', function(){
+      var simple = new Simple('+2000T12');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(undefined);
+      expect(simple.getDay()).to.equal(undefined);
+      expect(simple.getHours()).to.equal(12);
+      expect(simple.getMinutes()).to.equal(undefined);
+      expect(simple.getSeconds()).to.equal(undefined);
+    });
+
+    it('should parse +2000-01T12', function(){
+      var simple = new Simple('+2000-01T12');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(undefined);
+      expect(simple.getHours()).to.equal(12);
+      expect(simple.getMinutes()).to.equal(undefined);
+      expect(simple.getSeconds()).to.equal(undefined);
+    });
+
+    it('should parse +2000-01-01T24:00:00Z', function(){
+      var simple = new Simple('+2000-01-01T24:00:00Z');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(24);
+      expect(simple.getMinutes()).to.equal(0);
+      expect(simple.getSeconds()).to.equal(0);
+      expect(simple.getTZHours()).to.equal(0);
+      expect(simple.getTZMinutes()).to.equal(0);
+    });
+
+    it('should parse +2000-01-01T24:00:00+12', function(){
+      var simple = new Simple('+2000-01-01T24:00:00+12');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(24);
+      expect(simple.getMinutes()).to.equal(0);
+      expect(simple.getSeconds()).to.equal(0);
+      expect(simple.getTZHours()).to.equal(12);
+      expect(simple.getTZMinutes()).to.equal(undefined);
+    });
+
+    it('should parse +2000-01-01T24+12', function(){
+      var simple = new Simple('+2000-01-01T24+12');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(24);
+      expect(simple.getMinutes()).to.equal(undefined);
+      expect(simple.getSeconds()).to.equal(undefined);
+      expect(simple.getTZHours()).to.equal(12);
+      expect(simple.getTZMinutes()).to.equal(undefined);
+    });
+
+    it('should parse +2000-01-01T24:00+12', function(){
+      var simple = new Simple('+2000-01-01T24:00+12');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(24);
+      expect(simple.getMinutes()).to.equal(0);
+      expect(simple.getSeconds()).to.equal(undefined);
+      expect(simple.getTZHours()).to.equal(12);
+      expect(simple.getTZMinutes()).to.equal(undefined);
+    });
+
+    it('should error on +2000-01-01T24:00:00Z1', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00Z1');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T24:00:00+1', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00+1');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T24:00:00+24', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00+24');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T24:00:00+12-30', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00+12-30');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T24:00:00+12:3', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00+12:3');
+      }).to.throw(Error);
+    });
+
+    it('should parse +2000-01-01T24:00:00+12:30', function(){
+      var simple = new Simple('+2000-01-01T24:00:00+12:30');
+      expect(simple.getYear()).to.equal(2000);
+      expect(simple.getMonth()).to.equal(1);
+      expect(simple.getDay()).to.equal(1);
+      expect(simple.getHours()).to.equal(24);
+      expect(simple.getMinutes()).to.equal(0);
+      expect(simple.getSeconds()).to.equal(0);
+      expect(simple.getTZHours()).to.equal(12);
+      expect(simple.getTZMinutes()).to.equal(30);
+    });
+
+    it('should error on +2000-01-01T24:00:00+12:60', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00+12:60');
+      }).to.throw(Error);
+    });
+
+    it('should error on +2000-01-01T24:00:00+12:30B', function(){
+      expect(function() {
+        var simple = new Simple('+2000-01-01T24:00:00+12:30B');
+      }).to.throw(Error);
+    });
+
 /*
     it('should parse +2008-04-30T12:11:10', function(){
       var simple = new Simple('+2008-04-30');
@@ -265,6 +476,26 @@ describe('Simple', function(){
 
       expect(simple.getSeconds()).to.be.gte(date.getUTCSeconds()-1);
       expect(simple.getSeconds()).to.be.lte(date.getUTCSeconds()+1);
+    });
+
+  });
+
+  describe("#getTZHours()", function(){
+    it('should return the correct timezone offset', function(){
+
+      var simple = new Simple();
+
+      expect(simple.getTZHours()).to.equal(0);
+    });
+
+  });
+
+  describe("#getTZMinutes()", function(){
+    it('should return the correct timezone offset', function(){
+
+      var simple = new Simple();
+
+      expect(simple.getTZMinutes()).to.equal(0);
     });
 
   });
